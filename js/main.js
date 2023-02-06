35e3e5affa9b9aa7ec42d79e85d4655063389af9
rushi1222 <k.rushi1222@gmail.com>
2023-02-06 12:37:10 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$().ready(() => {
+    
+        $(".btn1").click(function(){
+            $(location).attr("href","cameras.html");
+        });
+     
+  
+        
+    })
+    
+        $().ready(() => {
+            $("#home").click(function(){
+                        $(location).attr("href","index.html");
+                    });
+        
+
+        })
+        $().ready(() => {
+            $("#about").click(function(){
+                        $(location).attr("href","about.html");
+                    });
+        
+
+        })
+        $().ready(() => {
+            $("#about1").click(function(){
+                        $(location).attr("href","about.html");
+                    });
+        
+
+        })
